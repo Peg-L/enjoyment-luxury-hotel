98ae973812aeed8dcef7a98eac8c930104a4d5e0
Peggy Lin <stu90233@gmail.com>
2024-11-28 23:08:56 +0800

    feat: 🎸 練習路由

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <FrontAppHeader />
+  <slot />
+  <FrontAppFooter />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ pages/member.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div>
+    <NuxtLayout name="user">
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+<style></style>
